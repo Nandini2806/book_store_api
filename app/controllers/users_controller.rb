e19c97c63eccdbf0fa198451a5d04a01bade7161
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-09-13 01:19:15 +0530

    Add 'role' in user to give  admin rights.
    Redefine appropriate routes, make changes in controllers accordingly and allow admins to do particular actions like update and delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:destroy]
 
-  def show
+  def books
     user = get_current_user
     purchase_books_id = BuyedBook.where(user_id: user.id)
     book_list = []
@@ -10,30 +9,28 @@ class UsersController < ApplicationController
       b = Book.find(book[:book_id])
       book_list.append({ book_id: b[:id], title: b[:title], author: b[:author] })
     end
-    render status: 200, json: book_list
+    render status: 200, json: { books_purchased: book_list }
   end
 
-  def show_all_reviews
+  def reviews
     user = get_current_user
     if user.reviewed_books.empty?
-      render status: 400, json: "You haven't added any reviews yet"
+      render status: 400, json: { message: "You haven't added any reviews yet" } 
     else
-      render status: 200, json: user.reviewed_books.select(:id, :book_id, :text)
+      render status: 200, json: { reviews: "#{user.reviewed_books.select(:id, :book_id, :text)}" }
     end
   end
 
   def destroy
-    del_user = delete_user_params
-    if User.destroy(User.find_by(del_user)[:id])
-      render status: 200, json: 'User successfully Deleted!'
+    user = get_current_user
+    if user.role == "admin"
+      if User.find(params[:id].split(',')).each do |user| user.destroy end
+        render status: 200, json: { message: 'User(s) successfully Deleted!' }
+      end
+    else
+      render status: 400, json: { message: "Invalid action! You are not an admin."}
     end
-  rescue NoMethodError
-    render status: 400, json: "User must exist!"
-  end
-
-  private
-
-  def delete_user_params
-    params.require(:user).permit(:user_name)
+  rescue ActiveRecord::RecordNotFound
+    render status: 400, json: { message: "User(s) must exist!" }
   end
 end
