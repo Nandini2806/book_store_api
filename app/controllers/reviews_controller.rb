1ec400f0363fcbd999a2f6713c22c228c8a86af5
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-09-12 15:20:24 +0530

    Modify controllers to directly fetch current user id instead of passing through post request and add routes to see all reviews of current user and all reviews of a book

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    review = ReviewedBook.new(reviewed_book_params)
+    user = get_current_user
+    review = ReviewedBook.new({
+      user_id: user.id,
+      book_id: reviewed_book_params[:book_id],
+      text: reviewed_book_params[:text]
+    })
     if review.save!
-      render status: 200, json: "Review for user #{review[:user_id]} and book #{review[:book_id]} is successfully added!"
-    else
-      render status: 400, json: "Could not add review!"
+      render status: 200, json: "Review for user #{user[:id]} and book #{review[:book_id]} is successfully added!"
     end
+  rescue ActiveRecord::RecordInvalid
+    render status 400, json: "You have already added review"
   end
 
   private
 
   def reviewed_book_params
-    params.require(:review).permit(:user_id, :book_id, :text)
+    params.require(:review).permit(:book_id, :text)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,37 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:destroy]
 
   def show
-    user = User.find_by(user_name_params)[:id]
-    purchase_books_id = BuyedBook.where(user_id: user)
+    user = get_current_user
+    purchase_books_id = BuyedBook.where(user_id: user.id)
     book_list = []
     purchase_books_id.each do |book|
       b = Book.find(book[:book_id])
-      book_list.append({ book_id: b[:id], title: b[:title] })
+      book_list.append({ book_id: b[:id], title: b[:title], author: b[:author] })
     end
-    render json: book_list
+    render status: 200, json: book_list
+  end
+
+  def show_all_reviews
+    user = get_current_user
+    if user.reviewed_books.empty?
+      render status: 400, json: "You haven't added any reviews yet"
+    else
+      render status: 200, json: user.reviewed_books.select(:id, :book_id, :text)
+    end
   end
 
   def destroy
     del_user = delete_user_params
     if User.destroy(User.find_by(del_user)[:id])
       render status: 200, json: 'User successfully Deleted!'
-    else
-      render status: 400, json: 'Could not delete user!'
     end
+  rescue NoMethodError
+    render status: 400, json: "User must exist!"
   end
 
   private
-  
-  def user_name_params
-    params.require(:user).permit(:user_name)
-  end
 
   def delete_user_params
     params.require(:user).permit(:user_name)
