6d1ebab983eb90cd0c0687b1a981d14f94da4fb2
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-09-12 15:18:09 +0530

    Fetch current logged in user and allow user_name, full_name while registering for new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
-class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
+include DeviseTokenAuth::Concerns::SetUserByToken
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def get_current_user
+    if user_signed_in?
+      return current_user
+    end
+  end
 
   def configure_permitted_parameters
     added_attrs = [:user_name, :full_name]
